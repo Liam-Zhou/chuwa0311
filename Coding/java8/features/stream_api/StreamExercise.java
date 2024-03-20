8aa150ff114653991937f679023d4e54f373b447
zjd1122 <jiadongzhang.c1@gmail.com>
2024-03-20 12:47:17 -0700

    HW3

---
+++ Coding/java8/features/stream_api/StreamExercise.java
@@ -0,0 +1,45 @@
+package com.chuwa.tutorial.t06_java8.features.stream_api;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author bigO
+ * @date 5/9/22 7:19 AM
+ * https://blog.devgenius.io/15-practical-exercises-help-you-master-java-stream-api-3f9c86b1cf82
+ * https://github.com/gavinklfong/stream-api-exercises
+ */
+public class StreamExercise {
+    @Test
+    public void testMapAndFlatMap() {
+        List<String> words = new ArrayList<String>();
+        words.add("hello");
+        words.add("word");
+        List<String> stringList = words.stream()
+                .flatMap(word -> Arrays.stream(word.split("")))
+                .distinct()
+                .collect(Collectors.toList());
+        stringList.forEach(e -> System.out.println(e));
+    }
+
+    @Test
+    public void testMapAndFlatMap2() {
+        List<String> words = new ArrayList<String>();
+        words.add("hello");
+        words.add("word");
+        List<Stream<String>> collect = words.stream()
+                .map(word -> Arrays.stream(word.split("")))
+                .distinct()
+                .collect(Collectors.toList());
+        collect.forEach(e -> System.out.println(e));
+        System.out.println("**** above are stream objects, below are character *****");
+        for (Stream<String> stringStream : collect) {
+            stringStream.forEach(System.out::println);
+        }
+    }
+}
