173c23c1c0437362903f189a0d8a60ff151abbbd
Haotian Wang <haotian_wang@hotmail.com>
2024-03-20 19:54:26 -0400

    stream API and optional practice

---
+++ Coding/java-core/src/main/java/com/chuwa/learn/java8/exercise/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.chuwa.learn.java8.exercise;
+import java.util.List;
+public class ShoppingCart {
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
