fd76f118a2083caf26c2a9790de7b2f593d28a2c
Yuchen Wang <kathyww12123@gmail.com>
2024-03-24 18:22:42 -0700

    add design pattern

---
+++ Repo/MavenProject/java-core/src/main/java/com/chuwa/learn/design_pattern/publisher_subscriber/Subject.java
@@ -0,0 +1,7 @@
+package com.chuwa.learn.design_pattern.publisher_subscriber;
+
+public interface Subject {
+    void attach(Observer observer);
+    void detach(Observer observer);
+    void notify(String message);
+}
